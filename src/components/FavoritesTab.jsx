9156f8efc42bc5766f9133e38e9f134e97924426
Ashot <ash.osip@gmail.com>
2018-10-15 18:54:12 +0400

    initial commit

---
+++ src/components/FavoritesTab.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class FavoritesTab extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return <h1>Hey hey favorite</h1>;
+	}
+}
+
+export default FavoritesTab;
